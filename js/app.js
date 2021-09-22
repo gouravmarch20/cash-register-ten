8977d046dc8adda61513658bbf29dcfe3d9944b9
Gourav Mishra <88934396+gouravmarch20@users.noreply.github.com>
2021-09-22 10:54:58 +0530

    add : css

--- js/app.js
+++ js/app.js
@@ -3,32 +3,23 @@ const givenAmount = document.querySelector('#given-amount')
 const calculateAmount = document.querySelector('#calculate-amount')
 const errorMessage = document.querySelector('#error-message')
 const noOfNotes = document.querySelectorAll('.no-of-notes')
-// console.log(noOfNotes);
-// noOfNotes[1].innerText = 34
-const cashTypeAvalible = [2000, 500, 200, 100, 10, 5, 1]
 
-calculateAmount.addEventListener(
-  'click',
-  function validateBillAndCashAmount () {
-    if (billAmount.value > 0) {
-    
-      if (givenAmount.value >= billAmount.value) {
-        const amountToBeReturned = givenAmount.value - billAmount.value
-        console.log(amountToBeReturned)
-        calculateChange(amountToBeReturned)
-      } else {
-        showMessage('Invalid bill  , require more amount ')
-      }
+const cashTypeAvalible = [2000, 500, 200, 100, 10]
+function validateBillAndCashAmount () {
+  if (billAmount.value > 0) {
+    if (Number(givenAmount.value) > Number(billAmount.value)) {
+      const amountToBeReturned = givenAmount.value - billAmount.value
+      calculateChange(amountToBeReturned)
     } else {
-      showMessage('Invalid bill amount , bill amount must greater than zero')
+      showMessage('Invalid bill  , require more amount ')
     }
+  } else {
+    showMessage('Invalid bill amount , bill amount must greater than zero')
   }
-)
-
+}
 function calculateChange (amountToBeReturned) {
   for (let i = 0; i < cashTypeAvalible.length; i++) {
     const numberOfNotes = Math.trunc(amountToBeReturned / cashTypeAvalible[i])
-    console.log(numberOfNotes)
     amountToBeReturned %= cashTypeAvalible[i]
     noOfNotes[i].innerText = numberOfNotes
   }
@@ -37,3 +28,5 @@ function showMessage (message) {
   errorMessage.style.display = 'block'
   errorMessage.innerText = message
 }
+
+calculateAmount.addEventListener('click', validateBillAndCashAmount)
--- test.js
+++
@@ -1,2 +0,0 @@
-const a = 5 - "2"
-console.log(typeof a);
